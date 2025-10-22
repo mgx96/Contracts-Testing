4dcc54b5d0053f5cf03b538aab28c33d3959d9c4
Malek Sharabi <m.malek1996@hotmail.com>
2025-10-22 10:47:18 +0200

    adding generic tests and modifying values accordingly

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -33,7 +33,7 @@ contract CreateSubscription is Script {
 }
 
 contract FundSubscription is Script, CodeConstants {
-    uint256 public constant FUND_AMOUNT = 3 ether;
+    uint256 public constant FUND_AMOUNT = 500 ether;
 
     function fundSubscriptionUsingConfig() public {
         HelperConfig helperConfig = new HelperConfig();
